51075d5e34822556a2a9eada1e83251d1e3ab2fa
Vinicius Port <caprilesport@gmail.com>
2025-03-17 20:40:50 -0300

    niri: gpu accelerate wezterm, enforce half width proportion

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,6 @@ config.color_scheme_dirs = { '/home/vport/.wezterm/' }
 config.window_decorations = 'NONE'
 config.enable_tab_bar = false
 config.default_prog = { '/usr/bin/zsh'}
+config.front_end = "WebGpu"
 
 return config
