d47011872f636056d0ed2b03055170539f6b25df
Vinicius Port <caprilesport@gmail.com>
2025-03-19 00:04:30 -0300

    Move to ayu mirage

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,11 +2,12 @@ local wezterm = require 'wezterm'
 local config = {}
 config.font = wezterm.font 'JetBrains Mono'
 
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "Ayu Mirage"
 config.color_scheme_dirs = { '/home/vport/.wezterm/' }
 config.window_decorations = 'NONE'
 config.enable_tab_bar = false
 config.default_prog = { '/usr/bin/zsh'}
 config.front_end = "WebGpu"
+config.max_fps = 240
 
 return config
