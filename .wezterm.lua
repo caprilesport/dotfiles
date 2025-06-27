f2f90a461a3758f8cd60e0f012c43851929f6c2c
Vinicius Port <caprilesport@gmail.com>
2025-06-26 21:21:31 -0300

    Maximize wezterm, index tmux starting on 1

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
 config.font = wezterm.font 'Fira Code'
+config.font_size = 13
 
 config.color_scheme = "Ayu Mirage"
 config.color_scheme_dirs = { '/home/vport/.wezterm/' }
@@ -9,6 +10,15 @@ config.enable_tab_bar = false
 config.default_prog = { '/usr/bin/fish'}
 -- config.front_end = "WebGpu"
 config.max_fps = 240
+
+wezterm.on("gui-startup",
+   function(cmd)
+      local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+      -- Maximise before toggling full screen:
+      window:gui_window():maximize()
+      window:gui_window():toggle_fullscreen()
+end
+)
 
 -- disable most of the keybindings because tmux can do that.
 -- in fact, I'm disabling all of them here and just allowing the few I want
