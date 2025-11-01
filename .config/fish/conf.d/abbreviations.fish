91e09180b0205ca84b48cb81b1813ca430a5da8a
Vinicius Port <caprilesport@gmail.com>
2025-11-01 17:59:28 -0300

    some small abbr changes and additions

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -32,6 +32,10 @@ abbr -a fakevpn 'sudo tailscale set --exit-node=trindatimes'
 abbr -a exitfakevpn 'sudo tailscale up --exit-node='
 abbr -a update-arch-mirrors 'rate-mirrors --disable-comments-in-file --entry-country=BR --protocol=https arch --max-delay 7200 | sudo tee /etc/pacman.d/mirrorlist'
 abbr -a update-eos-mirrors 'rate-mirrors --disable-comments-in-file --entry-country=BR --protocol=https endeavouros  | sudo tee /etc/pacman.d/endeavouros-mirrorlist'
+abbr -a activateagent 'eval (ssh-agent -c) && ssh-add ~/.ssh/id_ed25519'
+abbr -a getid "(recli status --show-id | fzf | awk '{print \$(NF)}')"
+abbr -a recli-ids "recli status --show-id | fzf  -m | awk '{print \$(NF)}'"
+abbr -a orca-normal 'rg "ORCA TERMINATED NORMALLY" --files-with-matches *.out '
 
 # programs 
 abbr -a ipy ipython
@@ -63,10 +67,10 @@ abbr -a cargoclippy 'cargo clippy --fix --allow-dirty --allow-staged -- -W clipp
 # git 
 abbr -a g git
 abbr -a gf 'git diff --name-only'
-abbr -a gac 'g a . && g c "updates" && g ps'
+abbr -a gac 'git a . && git c "updates" && git ps'
 
 #scripts environment
-abbr -a activatescripts '. ~/projects/scripts/.venv/bin/activate.fish'
+abbr -a pyenv '. ~/projects/scripts/.venv/bin/activate.fish'
 
 # orca stuff
 abbr -a energy 'grep -i "final gibbs free energy" **/*.out'
