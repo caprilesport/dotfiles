d9a0d59810bef7254da018ab80ed686b45eea986
Vinicius Port <caprilesport@gmail.com>
2026-04-01 11:00:22 -0300

    helix updates, remove the steel branch as of now

--- .config/helix/init.scm
+++
@@ -1,7 +0,0 @@
-; (require "smooth-scroll/smooth-scroll.scm")
-; (require "todo/main.scm")
-; (require "splash.scm")
-; (require "helix-ext.scm")
-
-; (when (equal? (command-line) '("hx"))
-;   (show-splash))
