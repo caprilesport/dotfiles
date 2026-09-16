57111573742061d2b3e4264e62ccd45cbe898b40
Vinicius Port <caprilesport@gmail.com>
2026-09-16 10:47:19 -0300

    niri: revamp config

--- .config/niri/scripts/idle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pkill -x swayidle 2>/dev/null || true
-
-exec swayidle -w \
-    timeout 600 'swaylock -f' \
-    timeout 1800 'swaylock -f && systemctl suspend' \
-    before-sleep 'swaylock -f'
